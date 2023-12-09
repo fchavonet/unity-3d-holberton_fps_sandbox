aac380d169373d1930f2ccfcc4e24ef97f39bb17
Fabien Chavonet <fchavonet@mailo.com>
2023-12-09 14:48:19 +0100

    Fix: PoolBehavior script error.

--- Assets/Scripts/PoolBehavior.cs
+++ Assets/Scripts/PoolBehavior.cs
@@ -7,11 +7,11 @@ public class PoolBehavior : MonoBehaviour
     // Called when the Collider of this object enters another Collider
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entering object is on the "PoolHoles" layer.
+        // Check if the entering object is on the "PoolHoles" layer
         if (other.gameObject.layer == LayerMask.NameToLayer("PoolHoles"))
         {
             // If the entering object has the correct layer, destroy this object
-            Destroy(gameObjet);
+            Destroy(gameObject);
         }
     }
 }
